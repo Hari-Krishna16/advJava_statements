37dba593b5460ed800981c9c40da8c927e866ac4
Hari-Krishna16 <134154620+Hari-Krishna16@users.noreply.github.com>
2023-07-19 18:04:21 +0530

    Add files via upload

--- QueryBuilder.java
+++ QueryBuilder.java
@@ -11,10 +11,11 @@ public class QueryBuilder {
 
 	public static void main(String[] args) {
 //		System.out.println(getInsertQuery("zerocode", "employee", Arrays.asList("pk_id", "name", "contact")));
-//		System.out.println(getUpdateQuery("zerocode", "employee", Arrays.asList("FirstName", "LastName"), " id"));
+		System.out.println(getUpdateQuery("zerocode", "employee", Arrays.asList("FirstName", "LastName"), " id","Hari"));
 //		System.out.println(getDeleteQuery("Zerocode", "employee","id"));
-//		System.out.println(getQuery("zerocode", "student", Arrays.asList(),"pk_id"));
-		System.out.println(getListQuery("zerocode", "district_table", Arrays.asList("employee", "roles")));
+//		System.out.println(getQuerys("zerocode", "student", Arrays.asList(), "pk_id", Arrays.asList("hari","shiva","mohan")));
+//		System.out.println(getListQuery("zerocode", "district_table", Arrays.asList("employee", "roles")));
+
 	}
 
 	/**
@@ -42,14 +43,14 @@ public class QueryBuilder {
 	/**
 	 * this method is used to generate query for update operations
 	 * 
-	 * @author hari
+	 * @author Hari Krishna
 	 * @param schema
 	 * @param tableName
 	 * @param columns
 	 * @param conditionColumn
 	 * @return Update Query
 	 */
-	public static String getUpdateQuery(String schema, String tableName, List<String> columns, String conditionColumn) {
+	public static String getUpdateQuery(String schema, String tableName, List<String> columns, String conditionColumn,Object value) {
 		if ((Utility.isBlank(schema) && Utility.isBlank(tableName)) && Utility.isBlank(columns)
 				&& Utility.isBlank(conditionColumn))
 			return null;
@@ -57,8 +58,12 @@ public class QueryBuilder {
 		for (String column : columns) {
 			columnNames += "," + GRAVE + column + GRAVE + " = ?";
 		}
+		if(value instanceof String) {
+			value = ",\""+value + "\"";
+			value= ((String) value).substring(1);
+		}
 		return "UPDATE " + GRAVE + schema + GRAVE + "." + GRAVE + tableName + GRAVE + " SET " + columnNames.substring(1)
-				+ " WHERE " + conditionColumn + " = ?" + ";";
+				+ " WHERE " + conditionColumn + " = "+value+";";
 	}
 
 	/**
@@ -78,13 +83,13 @@ public class QueryBuilder {
 	}
 
 	/**
-	 * this method is used to generate query for get the list of Column Data 
+	 * this method is used to generate query for get the list of Column Data
 	 * 
 	 * @author hari
 	 * @param schema
 	 * @param tableName
 	 * @param columns
-	 * @return select query 
+	 * @return select query
 	 */
 	public static String getListQuery(String schema, String tableName, List<String> columns) {
 		if ((Utility.isBlank(schema) && Utility.isBlank(tableName)) && Utility.isBlank(columns))
@@ -98,7 +103,7 @@ public class QueryBuilder {
 	}
 
 	/**
-	 * this method is used to generate query for get the row data 
+	 * this method is used to generate query for get the row data
 	 * 
 	 * @author hari
 	 * @param schema
@@ -107,11 +112,23 @@ public class QueryBuilder {
 	 * @param conditionColumn
 	 * @return select query
 	 */
-	public static String getQuery(String schema, String tableName, List<String> columns, String conditionColumn) {
+	public static String getQuery(String schema, String tableName, List<String> columns, String conditionColumn,
+			String values) {
 		if ((Utility.isBlank(schema) && Utility.isBlank(tableName)) && Utility.isBlank(columns))
 			return null;
 		String query = getListQuery(schema, tableName, columns);
-		return query.substring(0, query.length() - 1) + " WHERE " + conditionColumn + " = ?;";
+		return query.substring(0, query.length() - 1) + " WHERE " + conditionColumn + " IN (" + values + ") ;";
 	}
 
+	public static String getQuerys(String schema, String tableName, List<String> columns, String conditionColumn,
+			List<Object> values) {
+		if ((Utility.isBlank(schema)) && (Utility.isBlank(tableName)) && (Utility.isBlank(columns)))
+			return null;
+		String query = getListQuery(schema, tableName, columns);
+		String valueString ="";
+		for (Object value : values) {
+			valueString += (value instanceof String) ? ",\"" + value + "\"" : "," + value;
+		}
+		return query.substring(0, query.length() - 1) + " WHERE " + conditionColumn + " IN (" + (valueString.substring(1)) + ") ;";
+	}
 }
